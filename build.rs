9df8c103c9518aedbb1cd89fdf592053871a9ad0
Declan Soper <soperdeclan@gmail.com>
2019-04-09 23:14:33 +0100

    fix build.rs and generate new bindings

--- build.rs
+++ build.rs
@@ -1,6 +1,22 @@
 extern crate bindgen;
 
 use std::process::Command;
+use std::collections::HashSet;
+
+// IgnoreMacros courtesy of stillinbeta:
+// https://github.com/rust-lang/rust-bindgen/issues/687#issuecomment-450750547
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
 
 fn build_libembroidery() {
     Command::new("qmake")
@@ -20,12 +36,26 @@ fn main() {
     println!("cargo:rustc-link-search=./libembroidery/lib");
     println!("cargo:rustc-link-lib=embroidery");
 
-    let bindings = bindgen::Builder::default()
-        .header("libembroidery.h")
-        .generate()
-        .expect("Failed to build!");
+    // ignore oddly definited enums in math.h
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+            "IPPORT_RESERVED".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
 
-    bindings
+    bindgen::Builder::default()
+        .header("libembroidery.h")
+            .rustfmt_bindings(false)
+            .parse_callbacks(Box::new(ignored_macros))
+        .generate()
+            .expect("Failed to build!")
         .write_to_file("./src/bindings.rs")
-        .expect("Failed to write bindings");
+            .expect("Failed to write bindings!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!("bindings.rs");
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    // TODO: add tests
 }
