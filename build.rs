ff4ee27bd5271ac114daac5e23c91542c54ba8ef
Declan Soper <soperdeclan@gmail.com>
2019-04-13 18:19:21 +0100

    copy libembroidery to out_dir before build

--- build.rs
+++ build.rs
@@ -20,27 +20,57 @@ impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
     }
 }
 
-fn build_libembroidery(p: &str) {
+#[derive(Clone)]
+struct Paths {
+    out_path: PathBuf,
+    out_libpath: PathBuf,
+    manifest_path: PathBuf,
+    manifest_libpath: PathBuf,
+}
+
+fn config_paths() -> Paths {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let manifest_libpath = manifest_path.join("libembroidery");
+    let out_libpath = out_path.join("libembroidery");
+
+    Paths {
+        out_path,
+        out_libpath,
+        manifest_path,
+        manifest_libpath,
+    }
+}
+
+fn build_libembroidery(p: Paths) {
+    let manifest = p.manifest_path.to_str().unwrap();
+    let manifest_lib = p.manifest_libpath.to_str().unwrap();
+    let out_lib = p.out_libpath.to_str().unwrap();
+
+    Command::new("cp")
+        .args(&["-r", manifest_lib, out_lib])
+        .current_dir(manifest)
+        .output()
+        .expect("Failed to copy libembroidery to OUT_DIR");
     Command::new("qmake")
-        .current_dir(p)
+        .current_dir(out_lib)
         .output()
         .expect("Failed to build libembroidery: qmake");
     Command::new("make")
-        .current_dir(p)
+        .current_dir(out_lib)
         .output()
         .expect("Failed to build libembroidery: make");
 }
 
 fn main() {
-    let libembroidery_path =
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("libembroidery");
+    let p = config_paths();
 
-    build_libembroidery(libembroidery_path.to_str().unwrap());
+    build_libembroidery(p.clone());
     println!("Built libembroidery!");
 
     println!(
         "cargo:rustc-link-search={}",
-        libembroidery_path.join("lib").to_str().unwrap()
+        p.out_libpath.join("lib").to_str().unwrap()
     );
     println!("cargo:rustc-link-lib=static=embroidery");
 
@@ -58,13 +88,12 @@ fn main() {
         .collect(),
     );
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen::Builder::default()
         .header("libembroidery.h")
         .rustfmt_bindings(false)
         .parse_callbacks(Box::new(ignored_macros))
         .generate()
         .expect("Failed to build!")
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(p.out_path.join("bindings.rs"))
         .expect("Failed to write bindings!");
 }
